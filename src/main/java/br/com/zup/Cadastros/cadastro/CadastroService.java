45a0df462eb3020d00b33e743755d698826064f1
yan-rds <yyan1207@gmail.com>
2021-11-08 14:02:51 -0300

    Criar CadastroDTO

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroService.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Service;
 public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
+
+    public
 }
---
+++ src/main/java/br/com/zup/Cadastros/cadastro/dtos/CadastroDTO.java
@@ -0,0 +1,99 @@
+package br.com.zup.Cadastros.cadastro.dtos;
+
+import java.time.LocalDate;
+
+public class CadastroDTO {
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+    private String cidade;
+    private String bairro;
+    private String nomeDoParenteProximo;
+    private boolean moraSozinho;
+    private boolean temPet;
+    private int idade;
+    private LocalDate dataDoCadastro;
+
+    public CadastroDTO() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeDoParenteProximo() {
+        return nomeDoParenteProximo;
+    }
+
+    public void setNomeDoParenteProximo(String nomeDoParenteProximo) {
+        this.nomeDoParenteProximo = nomeDoParenteProximo;
+    }
+
+    public boolean isMoraSozinho() {
+        return moraSozinho;
+    }
+
+    public void setMoraSozinho(boolean moraSozinho) {
+        this.moraSozinho = moraSozinho;
+    }
+
+    public boolean isTemPet() {
+        return temPet;
+    }
+
+    public void setTemPet(boolean temPet) {
+        this.temPet = temPet;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public LocalDate getDataDoCadastro() {
+        return dataDoCadastro;
+    }
+
+    public void setDataDoCadastro(LocalDate dataDoCadastro) {
+        this.dataDoCadastro = dataDoCadastro;
+    }
+}
